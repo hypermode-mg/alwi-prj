e7f48bfbb8823a291d290f67f8f1f73512818b9c
Hyper Mode <hypermode@mail.ru>
2026-06-09 17:02:59 +0000

    Change cron settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,7 +28,7 @@ chmod 755 /var/log/apache2 /var/run/apache2
 # -----------------------------------------------------------------------------
 echo 'SHELL=/bin/bash' > /etc/cron.d/pingit
 echo 'BASH_ENV=/container.env' >> /etc/cron.d/pingit
-echo '*/5 * * * * root ( cd /var/www/html/modules/pingit/ && ./run-modules.sh ) >> /var/log/pingit.log 2>&1' >> /etc/cron.d/pingit
+echo '* * * * * root ( cd /var/www/html/modules/pingit/ && ./run-modules.sh ) >> /var/log/pingit.log 2>&1' >> /etc/cron.d/pingit
 echo '*/5 * * * * root ( cd /var/www/html/modules/nmapit/ && ./autonmap.sh ) >> /var/log/nmapit.log 2>&1' >> /etc/cron.d/nmapit
 chmod 644 /etc/cron.d/*
 
