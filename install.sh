8a6ac3286f8383861e7cbc6542477967245a4cbd
Hyper Mode <hypermode@mail.ru>
2026-06-10 10:39:59 +0000

    Change install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ TARGET_FILE2="web/install/step2.php"
 
 echo "TZ=Asia/Yekaterinburg" > $ENV_FILE
 echo "DB_HOST=alwi-db" >> $ENV_FILE
-echo "DB_USER=mysql-user" > $ENV_FILE
+echo "DB_USER=mysql-user" >> $ENV_FILE
 echo "DB_NAME=alertsonwings" >> $ENV_FILE
 
 while true; do
